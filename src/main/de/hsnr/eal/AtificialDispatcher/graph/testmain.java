bd22645aedc285b92640127be51733fb34993d1c
lammbraten <marius-brueckner@online.de>
2016-12-14 14:26:38 +0100

    Properties and Libraries

---
+++ src/main/de/hsnr/eal/AtificialDispatcher/graph/testmain.java
@@ -0,0 +1,14 @@
+package de.hsnr.eal.AtificialDispatcher.graph;
+
+import de.westnordost.osmapi.OsmConnection;
+
+public class testmain {
+
+	public static void main(String[] args) {
+		System.out.print("Hallo Welt");
+		
+		OsmConnection osm;
+
+	}
+
+}
