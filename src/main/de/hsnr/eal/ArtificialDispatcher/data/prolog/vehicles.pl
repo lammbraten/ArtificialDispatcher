a48ad4058e45d2cac0dd9a40f43dbecb62e65f69
lammbraten <marius-brueckner@online.de>
2016-12-27 18:01:17 +0100

    Prolog init

---
+++ src/main/de/hsnr/eal/ArtificialDispatcher/data/prolog/vehicles.pl
@@ -0,0 +1,9 @@
+% vehicle(Nr, Typ, Funk-Name)
+
+vehicle(000, 'ELW', 'ELW-1-1').
+vehicle(001, 'HLF-BF', 'HLF-1-1').
+vehicle(002, 'DLK-23', 'DLK-1-1').
+vehicle(003, 'HLF-BF', 'HLF-1-2').
+vehicle(004, 'RW2-K', 'RW-1-1').
+
+vehicle(005, 'HLF-BF'
